88d649d6f5312d77e133071ff8107df042e87941
Chimera <j.calderon3@gmail.com>
2021-01-06 11:12:03 -0500

    updted folder and script

--- index.js
+++ index.js
@@ -1,67 +1,73 @@
 // Beginning of script
-const inquirer = require('inquirer');
 const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // array of prompts for user
-inquirer.prompt([
+const questions = [
     {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your application?',
+      type: 'input',
+      name: 'github',
+      message: 'What is your GitHub username?',
     },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Please describe your application.',
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'What are the installation instructions for your application?',
-    },
-        {
-        type: 'input',
-        name: 'usage',
-        message: 'Is there any usage information for your application?',
-    },
-    {
-        type: 'input',
-        name: 'contributing',
-        message: 'Please state the guidelines for contributing to your application.',
-    },
-    {
-        type: 'input',
-        name: 'test',
-        message: 'What are the test instruction for your application?',
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+  },
+  {
+    type: 'input',
+    name: 'title',
+    message: "What is your project's name?",
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please write a short description of your project',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What kind of license should your project have?',
+    choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What command should be run to install dependencies?',
+    default: 'npm i',
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'What command should be run to run tests?',
+    default: 'npm test',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What does the user need to know about using the repo?',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'What does the user need to know about contributing to the repo?',
+  },
+];
 
-    },
-    {
-        type: 'input',
-        name: 'license',
-        choices: ['MIT', 'GPLv2', 'Apache', 'GPLv3'],
-        message: 'Which license did you use for your application?',
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'What is your Github username?',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Please provide the best email address to reach you?',
-    },
-])
+    // Function to write README file using the user input
+    function writeToFile(fileName, data) {
+      return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+    }
 
+    // Function to initialize app
+    function init() {
+      inquirer.prompt(questions).then((inquirerResponses) => {
+        console.log('Generating README...');
+        writeToFile('README.md', generateMarkdown({ ...inquirerResponses }));
+      });
+    }
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
-
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
-init();
+    // function call to initialize program
+    init();
